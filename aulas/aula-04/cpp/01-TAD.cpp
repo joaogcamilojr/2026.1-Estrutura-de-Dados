0d9b84cb7a3f103b825391a05fbbf72ae1011f29
João Gustavo Camilo Júnior <joaogcamilojr@gmail.com>
2026-03-12 20:18:48 -0300

    aula-4: TAD

---
+++ aulas/aula-04/cpp/01-TAD.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+void imprimir(int dados[], int tamanho) {
+    cout << "[";
+    for (int i = 0; i < tamanho; i++) {
+        cout << dados[i];
+        if (i < tamanho - 1) cout << ", ";
+    }
+    cout << "]" << endl;
+}
+
+void adicionar(int dados[], int &tamanho, int capacidade, int x) {
+    if (tamanho >= capacidade) return;
+    dados[tamanho] = x;
+    tamanho++;
+}
+
+void remover(int dados[], int &tamanho, int indice) {
+    if (indice < 0 || indice >= tamanho) return;
+    for (int i = indice; i < tamanho - 1; i++) {
+        dados[i] = dados[i + 1];
+    }
+    tamanho--;
+}
+
+int buscar(int dados[], int tamanho, int indice) {
+    if (indice < 0 || indice >= tamanho) return -1;
+    return dados[indice];
+}
+
+void inserirEm(int dados[], int &tamanho, int capacidade, int x, int indice) {
+    if (indice < 0 || indice > tamanho || tamanho >= capacidade) return;
+    for (int i = tamanho; i > indice; i--) {
+        dados[i] = dados[i - 1];
+    }
+    dados[indice] = x;
+    tamanho++;
+}
+
+int main() {
+    int dados[10] = {10, 20, 30};
+    int capacidade = 10;
+    int tamanho = 3;
+
+    imprimir(dados, tamanho);
+
+    adicionar(dados, tamanho, capacidade, 40);
+    imprimir(dados, tamanho);
+
+    remover(dados, tamanho, 2);
+    imprimir(dados, tamanho);
+
+    inserirEm(dados, tamanho, capacidade, 60, 1);
+    imprimir(dados, tamanho);
+
+    return 0;
+}
